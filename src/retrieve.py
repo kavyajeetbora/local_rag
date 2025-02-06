f64906a55a125246d671466475fac73d21617964
Kavyajeet Bora <kavyajeetbora@gmail.com>
2025-02-06 21:27:09 +0530

    Add - chroma

--- src/retrieve.py
+++ src/retrieve.py
@@ -1,11 +1,10 @@
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_ollama import ChatOllama
+from langchain_ollama import ChatOllama, OllamaEmbeddings
 
 from langchain_core.runnables import RunnablePassthrough
 from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain.vectorstores import Chroma
-
+from langchain_community.vectorstores import Chroma
 
 ## Load the model
 model = "deepseek-r1:1.5b" 
@@ -21,12 +20,40 @@ query_prompt = PromptTemplate(
     similarity search. Provide these alternative questions separated by newlines.
     Original question: {question}""",
 )
+embedding_model = OllamaEmbeddings(model="nomic-embed-text")
 
-vector_db = Chroma(persist_directory = r"db\chroma-vector-db-(rude-luminosity)")
-retriever = MultiQueryRetriever.from_llm(
-    vector_db.as_retriever(),
-    llm,
-    prompt=query_prompt
-)
+vector_db = Chroma(persist_directory = r"db\chroma-vector-db-(rude-luminosity)", embedding_function=embedding_model)
+query = "How many metro stations are there in India?"
 
+results = vector_db.similarity_search(query, k=5)
+print(len(results))
+
+
+# retriever = MultiQueryRetriever.from_llm(
+#     retriever = vector_db.as_retriever(search_kwargs={"k": 10}),
+#     llm = llm
+# )
+
+# query = "How many metro stations are there in India?"
+
+
+# ## Rag prompt
+# template = """
+# Answer the question based ONLY on the following context: 
+# {context}
+# Question:
+# {question}
+# """
+
+# query = "How many metro stations are there in India?"
+
+# prompt = ChatPromptTemplate.from_template(template)
+# print(prompt)
+
+# chain = (
+#     {"context": retriever, "question": RunnablePassthrough()}
+#     | prompt
+#     | llm
+#     | StrOutputParser()
+# )
 
